e5cd655f45519d65c0e2da32aa7df78d7e033787
congqin yan <1727053412@qq.com>
2023-05-04 10:35:21 +0100

    create springboot

---
+++ src/main/java/com/example/fizzbuz/FizzbuzApplication.java
@@ -0,0 +1,13 @@
+package com.example.fizzbuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FizzbuzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fizzbuz/FizzbuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fizzbuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
